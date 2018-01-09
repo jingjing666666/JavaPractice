52210344cf2fd8b3e34c9407c495997ee8b0fd81
袁勤静 <2953404091@qq.com>
2018-01-09 10:28:42 +0800

    redis使用

---
+++ src/basicgrammar/Student.java
@@ -0,0 +1,35 @@
+package basicgrammar;
+
+/**
+ * Created by jingjing on 2017/12/25.
+ */
+public class Student {
+    int score;
+    int number;
+    int state;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public int getScore() {
+
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
